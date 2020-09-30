a41725ffff25ab6b87208929297181078dc5a52a
Xuxinnuo <xuxinnuo@yahoo.com>
2020-10-01 00:09:36 +0100

    update get_annotation and merge_res

--- Human_eva/scripts/Extract/merge_res.py
+++ Human_eva/scripts/Extract/merge_res.py
@@ -1,55 +1,42 @@
 import json
 import os, sys
 
-def load_src(json_obj):
-    merge_ann = {}
-    for ex in json_obj:
-        example_id = ex["example_id"]
-        srcs = ex["srcs"]
-        if example_id in merge_ann:
-            continue
-        merge_ann[example_id] = {}
-        merge_ann[example_id]["srcs"] = srcs
-    return merge_ann
-
-def load_tgt(json_obj, merge_ann):
-    for ex in json_obj:
-        tgt_id = ex["tgt_id"]
-        tgt_tag = "tgt_"+str(tgt_id)
-        tgts = ex["tgts"]
-        sanity_check = ex["sanity_check"]
-        example_id = ex["example_id"]
-        merge_ann[example_id][tgt_tag] = {}
-        merge_ann[example_id][tgt_tag]["sanity_check"] = sanity_check
-        merge_ann[example_id][tgt_tag]["tgts"] = tgts
-        merge_ann[example_id][tgt_tag]["annotations"] = []
-        merge_ann[example_id][tgt_tag]["mtruk_code"] = []
-    return merge_ann
-
 def load_annotation(json_obj, merge_ann):
     for ex in json_obj:
         example_id = ex["example_id"]
-        tgt_id = ex["tgt_id"]
-        tgt_tag = "tgt_"+str(tgt_id)
-        annotation_res = ex["annotation_res"]
-        is_closed = ex["is_closed"]
-        is_filled = ex["is_filled"]
-        mtruk_code = ex["mtruk_code"]
-        if is_closed and is_filled:
+        srcs = ex["srcs"]
+        if example_id not in merge_ann:
+            merge_ann[example_id] = {}
+            merge_ann[example_id]["srcs"] = srcs
+        tgt_tag = "tgt_"+str(ex["tgt_id"])
+        sanity_check = ex["sanity_check"]
+        tgts = ex["tgts"]
+        if tgt_tag not in merge_ann[example_id]:
+            merge_ann[example_id][tgt_tag] = {}
+            merge_ann[example_id][tgt_tag]["sanity_check"] = sanity_check
+            merge_ann[example_id][tgt_tag]["tgts"] = tgts
+            merge_ann[example_id][tgt_tag]["annotations"] = []
+            merge_ann[example_id][tgt_tag]["mtruk_code"] = []
+        for ann in ex["annotations"]:
+            annotation_res = ann["annotation_res"]
+            mtruk_code = ann["mtruk_code"]
+            is_filled = ann["is_filled"]
+            #is_closed = ann["is_closed"]
+            #if is_closed and is_filled:
             merge_ann[example_id][tgt_tag]["annotations"].append(annotation_res)
             merge_ann[example_id][tgt_tag]["mtruk_code"].append(mtruk_code)
     return merge_ann
 
 if __name__ == '__main__':
-    for i in range(3): 
-        input_path = "../annotation_"+str(i)+".json"
+    merge_ann = {}
+    for filename in os.listdir('../'):
+        if not (filename.startswith('annotation') and filename.endswith('.json')):
+            continue
+        print (filename)
+        input_path = "../"+filename
         with open(input_path, 'r') as infile:
             json_obj = json.load(infile)
-        if i == 0:
-            merge_ann = load_src(json_obj)
-            merge_ann = load_tgt(json_obj, merge_ann)
         merge_ann = load_annotation(json_obj, merge_ann)
-
-    fpout = open("../annotation_all.json", "w")
+    fpout = open("../annotation.res", "w")
     fpout.write(json.dumps(merge_ann))
     fpout.close()
