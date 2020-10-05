c86ca141fcb656d95dfc41f02fca3aa39f9a20cb
xinnuo xu <xuxinnuo@yahoo.com>
2020-10-05 18:34:17 +0100

    move evluation scrip from data_webnlg to eva_webnlg

---
+++ eva_webnlg/evaluate_annotator.py
@@ -0,0 +1,101 @@
+#coding=utf8
+import os, sys
+from sklearn.metrics.cluster import normalized_mutual_info_score
+from scipy.stats import kendalltau
+from scipy.stats import spearmanr
+
+GT_PATH = './annotation_human/rst_ann_gt_plan.jsonl'
+SRC_PATH = './annotation_human/rst_ann_src.jsonl'
+
+class Percision(object):
+    def __init__(self, ANN_PATH):
+        self.ANN_PATH = ANN_PATH
+
+    def load_cand(self):
+        cands = []
+        for line in open(self.ANN_PATH):
+            flist = line.strip().split('\t')
+            sequence = flist[1].split('|')
+            ann_res = {}
+            for i, slot in enumerate(sequence):
+                types = slot.split('&&')
+                for t in types:
+                    ann_res[t] = i
+            cands.append(ann_res)
+        return cands
+
+    def load_cand_viterbi(self):
+        cands = {}
+        for line in open(self.ANN_PATH):
+            flist = line.strip().split('\t')
+            sequence = flist[1].split('|')
+            example_id = int(flist[2])
+            ann_res = {}
+            for i, slot in enumerate(sequence):
+                types = slot.split('&&')
+                for t in types:
+                    ann_res[t] = i
+            cands[example_id] = ann_res
+        return cands
+
+    def load_gold(self):
+        golds = []
+        for line in open(GT_PATH):
+            sequence = line.strip().split('|')
+            ann_res = {}
+            for i, slot in enumerate(sequence):
+                types = slot.split('&&')
+                for t in types:
+                    ann_res[t] = i
+            golds.append(ann_res)
+        return golds
+
+    def load_src(self):
+        srcs = []
+        for line in open(SRC_PATH):
+            srcs.append(line.strip().split('\t')[-1].split('|'))
+        return srcs
+
+    def get_acc(self, cands, golds, srcs, ignore_miss=False):
+        acc = 0; num = 0
+        for i, src in enumerate(srcs):
+            gold = golds[i]
+            cand = cands[i]
+            cand_ann = [cand[s] if s in cand else -1 for s in src]
+            gold_ann = [gold[s] for s in src]
+            for j in range(len(cand_ann)):
+                if cand_ann[j] == -1 and ignore_miss:
+                    continue
+                if cand_ann[j] == gold_ann[j]:
+                    acc += 1
+                num += 1
+        return acc/num
+
+    def run(self, is_viterbi=False, ignore_miss=False):
+        if is_viterbi:
+            cands = self.load_cand_viterbi()
+        else:
+            cands = self.load_cand()
+        golds = self.load_gold()
+        srcs = self.load_src()
+        acc = self.get_acc(cands, golds, srcs, ignore_miss)
+        print ('acc:', acc)
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == 'rule':
+        ANN_PATH = './annotation_human/webnlg_ann_tgt.jsonl'
+        acc_obj = Percision(ANN_PATH)
+        print ('Nomal:')
+        acc_obj.run(ignore_miss=False)
+        print ('Ignore miss:')
+        acc_obj.run(ignore_miss=True)
+    elif sys.argv[1] == 'viterbi':
+        ANN_PATH = '../logs/abs_bert_cnndm.30000.anno'
+        acc_obj = Percision(ANN_PATH)
+        print ('Nomal:')
+        acc_obj.run(is_viterbi=True, ignore_miss=False)
+        print ('Ignore miss:')
+        acc_obj.run(is_viterbi=True, ignore_miss=True)
+    else:
+        print ('ERROR: the parameter needs to be in [rule, viterbi]')
---
+++ eva_webnlg/evaluate_planning.py
@@ -0,0 +1,162 @@
+#coding=utf8
+import os, sys
+from sklearn.metrics.cluster import normalized_mutual_info_score
+from scipy.stats import kendalltau
+from scipy.stats import spearmanr
+
+CAND_HMM_PATH = '../logs/abs_bert_cnndm.30000.hmm'
+SRC_PATH = '../Human_eva/scripts/webnlg_ann_src.jsonl'
+GT_PATH = '../Human_eva/scripts/webnlg_ann_tgt.jsonl'
+
+class NMI(object):
+
+    def load_cand(self):
+        cands = []
+        for line in open(CAND_HMM_PATH):
+            flist = line.strip().split('\t')
+            types = flist[0].split()
+            pattern = flist[1].split()
+            group_id = 0
+            clustering = {}
+            for i in range(len(types)):
+                clustering[types[i]] = group_id
+                if pattern[i] == '1':
+                    group_id += 1
+            cands.append(clustering)
+        return cands
+
+    def load_gold(self):
+        golds = []
+        for line in open(GT_PATH):
+            flist = line.strip().split('\t')
+            one_example = []
+            for item in flist:
+                sequence = item.split('[XXN]')[1].split('|')
+                clustering = {}
+                for i, slot in enumerate(sequence):
+                    types = slot.split('&')
+                    for t in types:
+                        clustering[t] = i
+                one_example.append(clustering)
+            golds.append(one_example)
+        return golds
+
+    def load_src(self):
+        srcs = []
+        for line in open(SRC_PATH):
+            srcs.append(line.strip().split('\t')[-1].split('|'))
+        return srcs
+
+    def get_nmi(self, cands, golds, srcs):
+        cand_scores = []
+        ref_scores = []
+        for i, src in enumerate(srcs):
+            gold = golds[i]
+            cand = cands[i]
+            cand_sort = [cand[s] for s in src]
+
+            gold_sort = []
+            for g in gold:
+                gs = [g[s] for s in src]
+                gold_sort.append(gs)
+
+            tmp_scores = [normalized_mutual_info_score(gold, cand_sort) for gold in gold_sort]
+            cand_scores.append(max(tmp_scores))
+
+            if len(gold_sort) == 2:
+                ref_scores.append(normalized_mutual_info_score(gold_sort[0], gold_sort[1]))
+            elif len(gold_sort) == 3:
+                tmp_scores = []
+                tmp_scores.append(normalized_mutual_info_score(gold_sort[0], gold_sort[1]))
+                tmp_scores.append(normalized_mutual_info_score(gold_sort[0], gold_sort[2]))
+                tmp_scores.append(normalized_mutual_info_score(gold_sort[1], gold_sort[2]))
+                ref_scores.append(max(tmp_scores))
+        return sum(cand_scores)/len(cand_scores), sum(ref_scores)/len(ref_scores)
+
+    def run(self):
+        cands = self.load_cand()
+        golds = self.load_gold()
+        srcs = self.load_src()
+        cand_score, ref_score = self.get_nmi(cands, golds, srcs)
+        print ('cand_score:', cand_score)
+        print ('ref_score:', ref_score)
+
+
+class Kendalltau(object):
+
+    def load_cand(self):
+        cands = []
+        for line in open(CAND_HMM_PATH):
+            flist = line.strip().split('\t')
+            types = flist[0].split()
+            pattern = flist[1].split()
+            clustering = {}
+            for i in range(len(types)):
+                clustering[types[i]] = i
+            cands.append(clustering)
+        return cands
+
+    def load_gold(self):
+        golds = []
+        for line in open(GT_PATH):
+            flist = line.strip().split('\t')
+            one_example = []
+            for item in flist:
+                sequence = item.split('[XXN]')[1].split('|')
+                clustering = {}
+                for i, slot in enumerate(sequence):
+                    types = slot.split('&')
+                    for t in types:
+                        clustering[t] = i
+                one_example.append(clustering)
+            golds.append(one_example)
+        return golds
+
+    def load_src(self):
+        srcs = []
+        for line in open(SRC_PATH):
+            srcs.append(line.strip().split('\t')[-1].split('|'))
+        return srcs
+
+    def get_kendall(self, cands, golds, srcs):
+        cand_scores = []
+        ref_scores = []
+        for i, src in enumerate(srcs):
+            gold = golds[i]
+            cand = cands[i]
+            cand_sort = [cand[s] for s in src]
+
+            gold_sort = []
+            for g in gold:
+                gs = [g[s] for s in src]
+                gold_sort.append(gs)
+
+            tmp_scores = [kendalltau(gold, cand_sort)[0] for gold in gold_sort]
+            cand_scores.append(max(tmp_scores))
+
+            if len(gold_sort) == 2:
+                ref_scores.append(kendalltau(gold_sort[0], gold_sort[1])[0])
+            elif len(gold_sort) == 3:
+                tmp_scores = []
+                tmp_scores.append(kendalltau(gold_sort[0], gold_sort[1])[0])
+                tmp_scores.append(kendalltau(gold_sort[0], gold_sort[2])[0])
+                tmp_scores.append(kendalltau(gold_sort[1], gold_sort[2])[0])
+                ref_scores.append(max(tmp_scores))
+        return sum(cand_scores)/len(cand_scores), sum(ref_scores)/len(ref_scores)
+
+    def run(self):
+        cands = self.load_cand()
+        golds = self.load_gold()
+        srcs = self.load_src()
+        cand_score, ref_score = self.get_kendall(cands, golds, srcs)
+        print ('cand_score:', cand_score)
+        print ('ref_score:', ref_score)
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == 'group':
+        nmi_obj = NMI()
+        nmi_obj.run()
+    elif sys.argv[1] == 'sort':
+        k_obj = Kendalltau()
+        k_obj.run()
